51757d2e1d2ca0d528a9c763a65375fa615a9bd1
Husky4k <156296710+Husky4k@users.noreply.github.com>
2024-03-10 07:55:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
